aa2d710bdf71b715f310fb05f3412cbccc3204a0
Matvdg <matvdg@me.com>
2016-10-22 22:13:05 +0200

    fixed photo rotation bug
    rotation is just a metadata since iOS 4 -> JPEGRepresentation takes metadata, not PNGRepresentation -> so we graphically rotate if necessary before saving into PNG, UIImage extension
    
    close #17

---
+++ WooShot/ChatViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ChatViewController.swift
+//  WooShot
+//
+//  Created by Mathieu Vandeginste on 22/10/2016.
+//  Copyright © 2016 WooShot. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+    
+}
+
+
